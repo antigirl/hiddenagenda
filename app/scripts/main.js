6b7b0131c1c87f16d3892ff51f65384bb545b9bc
Farz <farzad.qasim@gmail.com>
2014-02-17 18:34:47 +0000

    scrolling changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,73 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(function() {
+    var offset1 = 0;
+    var offset2 = 0;
+    var offset3 = 0;
+
+
+    $(window).bind("scroll", function(event) {
+
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+
+        var eleOffset1 = $('#first').offset().top;
+        var img1 = eleOffset1 + $('#first').height();
+
+        offset1 = 400 - ($('body').scrollTop() * 0.5);
+        $('.parallaxWrapper .img1').css('-webkit-transform', 'translate3d(0,' + offset1 + 'px,0)');
+
+
+        if (docViewTop - eleOffset1 >= 0) {
+            $('.parallaxWrapper .img2').css('-webkit-transform', 'translate3d(0,-90000px,0)');
+        }
+
+
+        var eleOffset2 = $('#second').offset().top;
+        var img2 = eleOffset2 + $('#second').height();
+
+
+        if (docViewBottom >= img2) {
+            offset2 = 400 - (($('body').scrollTop() * 0.5) - ((850+500)/2));
+            $('.parallaxWrapper .img2').css('-webkit-transform', 'translate3d(0,' + offset2 + 'px,0)');
+        }
+
+        if (docViewTop - eleOffset2 >= 0) {
+             $('.parallaxWrapper .img3').css('-webkit-transform', 'translate3d(0,-90000px,0)');
+        }
+
+
+        var eleOffset3 = $('#third').offset().top;
+        var img3 = eleOffset3 + $('#third').height();
+
+        if (docViewBottom >= img3) {
+            offset3 = 400 - (($('body').scrollTop() * 0.5) - ((850+500+850+500)/2));
+            $('.parallaxWrapper .img3').css('-webkit-transform', 'translate3d(0,' + offset3 + 'px,0)');
+        }
+
+        if (docViewTop - eleOffset3 >= 0) {
+            $('.parallaxWrapper .img4').css('-webkit-transform', 'translate3d(0,-90000px,0)');
+        }
+    });
+});
+
+
+
 // //A very simple parallax effect
 // window.addEventListener('scroll', function() {
 
@@ -19,48 +89,48 @@
 //      $('article').css('-webkit-transform',  'translate3d(0,'+ parseInt($('body').scrollTop() * 0.8,10) + 'px, 0)');
 // }); 
 
-$(function() {
-    var offset = 0;
-    var message = '';
-    $(window).bind("scroll", function(event) {
-    	offset =  parseInt($('body').scrollTop() * 0.1,	10)
-        $('.parallaxWrapper img:nth-child(1)').css('-webkit-transform','translate3d(0,'+  offset +'px,0)');
-        // $(".blocks:below-the-fold").each(function() {
-        //  	 message = $(this).attr("id");
-        // });
+// $(".blocks:below-the-fold").each(function() {
+//     message = $(this).attr("id");
+// });
 
-        	//var windows = document.documentElement.scrollTop;
-
-    	var eleOffset = $('#second').offset().top;
-    	var img2 = eleOffset + $('#second').height();
-
-    	var docViewTop = $(window).scrollTop();
-  		var docViewBottom = docViewTop + $(window).height();
-    	 	// console.log(windows);
-    	if(docViewBottom >= img2) {
-        	//console.log('start moving third parallax image');
-        		var offset2 =  $(window).height() - parseInt($('body').scrollTop() * 0.6, 10);
-        	$('.parallaxWrapper img:nth-child(2)').css('-webkit-transform','translate3d(0,'+  offset2 +'px,0)');
-       	}
-
-       	if(docViewTop - eleOffset >= 0 ) {
-       		// $('.parallaxWrapper img:nth-child(1)').css('-webkit-transform','translate3d(0,-90000px,0)');
-       		//console.log('-9000px 2nd parallax image')
-       	}
+//var windows = document.documentElement.scrollTop;
 
 
-    	var eleOffset3 = $('#third').offset().top;
-    	var img3 = eleOffset3 + $('#third').height();
+// offset = parseInt($('body').scrollTop() * 0.1, 10)
+// $('.parallaxWrapper img:nth-child(1)').css('-webkit-transform', 'translate3d(0,' + offset + 'px,0)');
 
-    	if(docViewBottom >= img3) {
-        	console.log('start moving third parallax image');
-        		var offset3 =  $(window).height()  - parseInt($('body').scrollTop() * 0.3, 10);
-        	$('.parallaxWrapper img:nth-child(3)').css('-webkit-transform','translate3d(0,'+  offset3 +'px,0)');
-       	}
 
-       	if(docViewTop - eleOffset3 >= 0 ) {
-       		// $('.parallaxWrapper img:nth-child(1)').css('-webkit-transform','translate3d(0,-90000px,0)');
-       	}       	
+// var eleOffset = $('#second').offset().top;
+// var img2 = eleOffset + $('#second').height();
 
-    });
-});
+// var docViewTop = $(window).scrollTop();
+// var docViewBottom = docViewTop + $(window).height();
+
+// if (docViewBottom >= img2) {
+
+//     //  console.log('second image is in view');
+//     var offset2 = $(window).height() - parseInt($('body').scrollTop() * 0.5, 10);
+//     var offset2a = -(($('body').scrollTop() - $('#second').offset().top) - 100) * 0.2;
+//     offset3a = offset3a - 1;
+//     console.log(offset3a);
+//     offsetz = -parseInt($('body').scrollTop() * 0.1, 10)
+//     $('.parallaxWrapper img:nth-child(2)').css('-webkit-transform', 'translate3d(0,' + offsetz + 'px,0)');
+// }
+
+// if (docViewTop - eleOffset >= 0) {
+//     $('.parallaxWrapper img:nth-child(1)').css('-webkit-transform', 'translate3d(0,-90000px,0)');
+//     //    console.log('second image is reached head');
+// }
+
+
+// var eleOffset3 = $('#third').offset().top;
+// var img3 = eleOffset3 + $('#third').height();
+// var offset3a = -(($('body').scrollTop() - $('#third').offset().top) - 100) * 0.2;
+// if (docViewBottom >= img3) {
+//     offsetz = -parseInt($('body').scrollTop() * 0.1, 10)
+//     $('.parallaxWrapper img:nth-child(3)').css('-webkit-transform', 'translate3d(0,' + offsetz + 'px,0)');
+// }
+
+// if (docViewTop - eleOffset3 >= 0) {
+//     $('.parallaxWrapper img:nth-child(2)').css('-webkit-transform', 'translate3d(0,-90000px,0)');
+// }
